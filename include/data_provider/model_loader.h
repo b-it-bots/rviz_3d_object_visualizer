b49987f824da73dc67760b7be8262c3ecb1f7a5c
Sushant Chavan <gitecsvc@gmail.com>
2020-07-26 17:19:12 +0200

    Setup an empty 3D model loader shared library

---
+++ include/data_provider/model_loader.h
@@ -0,0 +1,11 @@
+#ifndef DATA_PROVIDER
+#define DATA_PROVIDER
+
+class ModelLoader
+{
+public:
+    ModelLoader();
+    virtual ~ModelLoader(){}
+};
+
+#endif // DATA_PROVIDER
---
+++ src/data_provider/model_loader.cpp
@@ -0,0 +1,7 @@
+#include "data_provider/model_loader.h"
+#include <iostream>
+
+ModelLoader::ModelLoader()
+{
+    std::cout << "Model Loader object created" << std::endl;
+}
