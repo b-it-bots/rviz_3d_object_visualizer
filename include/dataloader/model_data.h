30bf40cc731851947b095f09bec78fb0c1764bc5
Sushant Chavan <gitecsvc@gmail.com>
2020-08-07 10:57:20 +0200

    Update the ModelData Class to use the utility functions to store the model related data

--- include/dataloader/model_data.h
+++ include/dataloader/model_data.h
@@ -1,14 +1,31 @@
 #ifndef MODEL_DATA
 #define MODEL_DATA
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+
+#include "dataloader/utils.h"
+#include "dataloader/model.h"
+
 namespace RVizDataLoader 
 {
     class ModelData 
     {
     public:
-        ModelData();
+        ModelData(){}
         virtual ~ModelData(){}
 
+        Utils::Pose<double> pose_;
+        Utils::Vec3<double> scale_;
+        Utils::Vec3<int> color_;
+        std::string mesh_resource_;
+        bool use_color_from_mesh_;
+
+        Model::Types type_;
     };
 };
 
