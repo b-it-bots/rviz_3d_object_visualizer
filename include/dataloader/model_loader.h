b0b0118f41e0188d36fc95e367a0d414dbabe9cf
Sushant Chavan <gitecsvc@gmail.com>
2020-08-07 11:01:34 +0200

    Implement the ModelLoader class

--- include/dataloader/model_loader.h
+++ include/dataloader/model_loader.h
@@ -2,8 +2,9 @@
 #define MODEL_LOADER
 
 #include<string>
-#include <memory>
+#include<memory>
 
+#include "dataloader/yaml_loader.h"
 #include "dataloader/model_data.h"
 
 namespace RVizDataLoader
@@ -11,11 +12,21 @@ namespace RVizDataLoader
     class ModelLoader
     {
     public:
-        ModelLoader();
+        ModelLoader(const std::string& model_config_path);
         virtual ~ModelLoader(){}
 
+        auto getMarker(int id,
+                       Model::Types type, 
+                       const std::string& frame_id, 
+                       const std::string& ns)
+                       -> std::unique_ptr<visualization_msgs::Marker>;
+
+    protected:
         // TODO: Make this function pure virtual
-        virtual std::unique_ptr<ModelData> loadModel(const std::string& model_file_path){}
+        virtual auto loadModel(Model::Types model_type)
+                              -> std::unique_ptr<visualization_msgs::Marker>;
+
+        YamlLoader yaml_loader_;
     };
 };
 
--- src/dataloader/model_loader.cpp
+++ src/dataloader/model_loader.cpp
@@ -1,10 +1,71 @@
 #include <iostream>
 
+#include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 #include "dataloader/model_loader.h"
 
 using namespace RVizDataLoader;
 
-ModelLoader::ModelLoader()
+ModelLoader::ModelLoader(const std::string& model_config_path)
+: yaml_loader_(model_config_path)
 {
-    std::cout << "Model Loader object created" << std::endl;
+}
+
+auto ModelLoader::loadModel(Model::Types model_type) 
+                            -> std::unique_ptr<visualization_msgs::Marker>
+{
+    std::unique_ptr<visualization_msgs::Marker> marker = std::unique_ptr<visualization_msgs::Marker>(new visualization_msgs::Marker);
+
+    ModelData model_data = yaml_loader_.getModelConfig(model_type);
+
+    marker->type = visualization_msgs::Marker::MESH_RESOURCE;
+    marker->pose.position.x = model_data.pose_.position.x();
+    marker->pose.position.y = model_data.pose_.position.y();
+    marker->pose.position.z = model_data.pose_.position.z();
+
+    tf2::Quaternion quat;
+    quat.setRPY(model_data.pose_.orientation.roll(), 
+                model_data.pose_.orientation.pitch(), 
+                model_data.pose_.orientation.yaw());
+    quat.normalize();
+    marker->pose.orientation = tf2::toMsg(quat);
+
+    marker->scale.x = model_data.scale_.x();
+    marker->scale.y = model_data.scale_.y();
+    marker->scale.z = model_data.scale_.z();
+
+    if (model_data.use_color_from_mesh_)
+    {
+        marker->color.r = 0.0;
+        marker->color.g = 0.0;
+        marker->color.b = 0.0;
+        marker->color.a = 0.0;
+        marker->mesh_use_embedded_materials = true;
+    }
+    else
+    {
+        marker->color.r = model_data.color_.r() / 255.0;
+        marker->color.g = model_data.color_.g() / 255.0;
+        marker->color.b = model_data.color_.b() / 255.0;
+        marker->color.a = 1.0;
+        marker->mesh_use_embedded_materials = false;
+    }
+
+    marker->mesh_resource = model_data.mesh_resource_;
+
+    return std::move(marker);
+}
+
+auto ModelLoader::getMarker(int id, Model::Types type, const std::string& frame_id, const std::string& ns)
+               -> std::unique_ptr<visualization_msgs::Marker>
+{
+    std::unique_ptr<visualization_msgs::Marker> marker = loadModel(type);
+    marker->header.frame_id = frame_id;
+    marker->header.stamp = ros::Time();
+    marker->ns = ns;
+    marker->id = id;
+
+    return std::move(marker);
 }
