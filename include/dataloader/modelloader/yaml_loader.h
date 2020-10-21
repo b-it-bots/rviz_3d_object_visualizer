79ab0511afcf87c2b650ec9488bc3cf4ecba935e
Sushant Chavan <gitecsvc@gmail.com>
2020-10-21 19:51:07 +0200

    Update/Add file descriptions for header and source files

--- include/dataloader/modelloader/yaml_loader.h
+++ include/dataloader/modelloader/yaml_loader.h
@@ -1,3 +1,34 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2020 Ahmed Faisal Abdelrahman, Sushant Vijay Chavan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+/**
+  File: yaml_loader.h
+  Purpose: Utility class to load mesh configurations from a YAML file.
+  @author Sushant Vijay Chavan
+  @version 1.0 16/10/20
+*/
+
 #ifndef DATALOADER_YAML_LOADER
 #define DATALOADER_YAML_LOADER
 
