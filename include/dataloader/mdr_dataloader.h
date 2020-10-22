1211a8729af194767c2e2d5d216f0287ae9c58bf
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2020-10-22 16:19:52 +0200

    Add license comments

--- include/dataloader/mdr_dataloader.h
+++ include/dataloader/mdr_dataloader.h
@@ -1,22 +1,39 @@
-/**
-    File: mdr_dataloader.h
-    Purpose: Loader for b-it-bots MAS MongoDB data
-    @author Ahmed Faisal Abdelrahman
-    @author Sushant Vijay Chavan
-    @version 1.0
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2020 Ahmed Faisal Abdelrahman, Sushant Vijay Chavan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
 */
 
 #ifndef MDR_DATALOADER_H
 #define MDR_DATALOADER_H
 
 #include <ros/ros.h>
-#include "dataloader/abstract_dataloader.h"
-#include "dataloader/modelloader/model_loader.h"
-#include "dataloader/modelloader/model_data.h"
 
 #include <mas_perception_msgs/Object.h>
 #include <mas_perception_msgs/Person.h>
 #include <mas_perception_msgs/Plane.h>
+
+#include "dataloader/abstract_dataloader.h"
+#include "dataloader/modelloader/model_loader.h"
+#include "dataloader/modelloader/model_data.h"
 
 namespace RVizDataLoader 
 {
